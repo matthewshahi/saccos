875421d6002726b4e846406c56a203aa1d1cf62f
Matthew Shahi <matthewshahi@gmail.com>
2024-11-17 09:45:28 +0300

    898

--- resources/views/partials/menu_public.blade.php
+++ resources/views/partials/menu_public.blade.php
@@ -1,7 +1,8 @@
 <div class="sidebar-panel bg-white">
     <div class="gull-brand pe-3 text-center mt-4 mb-2 d-flex justify-content-center align-items-center">
         <a href="{{ url('/') }}" style="text-decoration: none;">
-            <span style="margin-left: 10px; font-size: 33px; color: rebeccapurple; font-weight: 900; font-family: 'Montserrat', sans-serif; background: linear-gradient(to right, rebeccapurple, indigo); -webkit-background-clip: text; color: transparent; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);">iSacco</span>
+            <span style="margin-left: 10px; font-size: 33px; color: rebeccapurple; font-weight: 900; font-family: 'Montserrat', sans-serif; background: linear-gradient(to right, rebeccapurple, indigo); -webkit-background-clip: text; color: transparent; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);">
+             {{ $defaultCompanyName }}   </span>
         </a>
         <div class="sidebar-compact-switch ms-auto"><span></span></div>
     </div>
@@ -15,7 +16,7 @@
                     <li class="Ul_li--hover">
     <a href="{{ url('/register') }}">
        
-        <span class="item-name text-15 text-muted">Register (Non Members)</span>
+        <span class=" text-muted">Register (Non Members)</span>
     </a>
 </li>    
 <li class="item-name">
