68f002bef336b93682f99b3d9aeecabc3848b71a
Matthew Shahi <matthewshahi@gmail.com>
2024-11-17 21:06:13 +0300

    finals

--- resources/views/partials/menu_public.blade.php
+++ resources/views/partials/menu_public.blade.php
@@ -20,12 +20,12 @@
     </a>
 </li>    
 <li class="item-name">
-                                <a href="{{ url('/') }}">
+                                <a href="{{ url('/loans/types/list') }}">
                                     <span class="text-muted">Available Loans</span>
                                 </a>
                             </li>   
                             <li class="item-name">
-                                <a href="{{ url('/loans/types/list') }}">
+                                <a href="{{ url('/') }}">
                                     <span class="text-muted">Login (Members Only)</span>
                                 </a>
                             </li>              
