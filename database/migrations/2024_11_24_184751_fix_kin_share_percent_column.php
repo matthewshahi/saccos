a648c79290359ffab54667b17f3799aa3340a01b
Matthew Shahi <matthewshahi@gmail.com>
2024-11-24 21:53:42 +0300

    45678

--- database/migrations/2024_11_24_184751_fix_kin_share_percent_column.php
+++ database/migrations/2024_11_24_184751_fix_kin_share_percent_column.php
@@ -3,7 +3,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FixKinSharePercentInSaccoMembersTable extends Migration
+ 
+class FixKinSharePercentInSaccoMembersTables extends Migration
 {
     /**
      * Run the migrations.
