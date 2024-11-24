f99927f642258d8adf57274728531f6e80a51973
Matthew Shahi <matthewshahi@gmail.com>
2024-11-24 21:31:39 +0300

    45678

--- app/Http/Controllers/PublicRegistrationController.php
+++ app/Http/Controllers/PublicRegistrationController.php
@@ -70,6 +70,7 @@ class PublicRegistrationController extends Controller
         $recaptchaSuccess = $recaptchaData['success'] ?? false;
         $recaptchaScore = $recaptchaData['score'] ?? 0;
 
+        dd($recaptchaScore);
         if (!$recaptchaSuccess || $recaptchaScore < 0.5) { // 0.5 threshold for spam
             return redirect()->back()->withErrors(['captcha' => 'reCAPTCHA verification failed or score too low.'])->withInput();
         }
