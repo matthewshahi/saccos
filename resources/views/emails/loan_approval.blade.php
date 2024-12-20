d14e602f975f374d0006b7448d7d4e46ff7bc146
Matthew Shahi <matthewshahi@gmail.com>
2024-12-20 14:14:32 +0300

    ‘mpesa’

--- resources/views/emails/loan_approval.blade.php
+++ resources/views/emails/loan_approval.blade.php
@@ -40,10 +40,10 @@
         <p>Congratulations! Your loan has been approved. Here are the details:</p>
         <ul>
             <li><strong>Loan Type:</strong> {{ $loan->loan_type }}</li>
-            <li><strong>Amount:</strong> {{ number_format($loan->loan_amount, 2) }}</li>
-            <li><strong>Repayment Period:</strong> {{ $loan->loan_taken_period }} months</li>
-            <li><strong>Monthly Repayment:</strong> {{ number_format($loan->loan_monthly_repayment_amount, 2) }}</li>
-        </ul>
+            <li><strong>Loan Amount:</strong> Ksh {{ number_format($loan->loan_amount, 2) }}</li>
+            <li><strong>Repayment Period:</strong> {{ $loan->loan_payment_period }} months</li>
+            <li><strong>Monthly Repayment:</strong> Ksh {{ number_format($loan->loan_monthly_repayment_amount, 2) }}</li>
+         </ul>
 
         @if (!empty($saccoMail) && filter_var($saccoMail, FILTER_VALIDATE_EMAIL))
             <p>If you have any questions or need support, feel free to contact us at <a href="mailto:{{ $saccoMail }}">{{ $saccoMail }}</a>.</p>
