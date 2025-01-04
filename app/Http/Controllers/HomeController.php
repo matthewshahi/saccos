2c0f1f89d98e020c8775c33fac8b5c7b745c2023
Matthew Shahi <matthewshahi@gmail.com>
2025-01-04 21:02:44 +0300

    8

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3535,6 +3535,8 @@ public function submitLoanApplication(Request $request)
                     ->where('guarantors_deleted', '<>', 'Y')
                     ->sum('guarantors_amount_guaranteed') ?? 0;
 
+                    $nmsg .= "Error, guarantor {$guarantorName}, in row " . ($i + 1) . " has over guaranteed. ".($guarantor->member_total_share * $max_guarantor_factor - $guarantor->member_tied_shares)."-- ".($guarantorAmount);
+                    dd($nmsg);
                 if (($guarantor->member_total_share * $max_guarantor_factor - $guarantor->member_tied_shares) < ($guarantorAmount)) {
                     $nmsg .= "Error, guarantor {$guarantorName}, in row " . ($i + 1) . " has over guaranteed. ".($guarantor->member_total_share * $max_guarantor_factor - $guarantor->member_tied_shares)."-- ".($guarantorAmount);
                     dd($nmsg);
