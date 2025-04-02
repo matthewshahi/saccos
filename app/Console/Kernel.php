26485e9ddd24f493c52f83b88b150e39b08705f3
Matthew Shahi <matthewshahi@gmail.com>
2025-04-02 10:12:33 +0300

    guarantors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,23 +12,23 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
 {
-    $schedule->job(new \App\Jobs\ProcessTransactionsJob())
+   /**  $schedule->job(new \App\Jobs\ProcessTransactionsJob())
         ->everyMinute()
         ->withoutOverlapping();
 
-    // $schedule->job(new \App\Jobs\SendWelcomeEmailJob())
-    //     ->everyMinute()
-    //     ->withoutOverlapping();
+    $schedule->job(new \App\Jobs\SendWelcomeEmailJob())
+        ->everyMinute()
+        ->withoutOverlapping();
 
-    // $schedule->job(new \App\Jobs\SendGuarantorEmailJob())
-    //     ->everyMinute()
-    //     ->withoutOverlapping();
+    $schedule->job(new \App\Jobs\SendGuarantorEmailJob())
+        ->everyMinute()
+        ->withoutOverlapping();
 
-    // $schedule->job(new \App\Jobs\ProcessLoanEmailsJob())
-    //     ->everyTenMinutes()
-    //     ->withoutOverlapping();
+    $schedule->job(new \App\Jobs\ProcessLoanEmailsJob())
+        ->everyTenMinutes()
+        ->withoutOverlapping();*/
 
-    $schedule->job(new \App\Jobs\ResetGuarantorsJob())->dailyAt('00:00')->withoutOverlapping();
+    $schedule->job(new \App\Jobs\ResetGuarantorsJob())->everyMinute()->withoutOverlapping();
 
 }
 
