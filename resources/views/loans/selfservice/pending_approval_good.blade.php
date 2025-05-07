0ec96dedf35c81b309d50dd59cc58a7975006135
Matthew Shahi <matthewshahi@gmail.com>
2025-05-07 12:54:32 +0300

    supply chain

--- resources/views/loans/selfservice/pending_approval_good.blade.php
+++ resources/views/loans/selfservice/pending_approval_good.blade.php
@@ -223,7 +223,9 @@ function confirmAction(action, loanId) {
             document.getElementById(`rejectForm${loanId}`).submit();
         }
     }
+    
 }
+
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
