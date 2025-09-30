2e95b77436fb9d7c2bbd53b7893dc01e2b37a131
Matthew Shahi <matthewshahi@gmail.com>
2025-09-30 11:47:02 +0300

    members

--- routes/web.php
+++ routes/web.php
@@ -44,17 +44,17 @@ use App\Http\Controllers\TxnImportController;
 // use App\Http\Controllers\TxnImportController;
 
 // // Upload form
-Route::get('/transactions/import', [TxnImportController::class, 'showForm'])
-    ->name('transactions.import.form');
+// Route::get('/transactions/import', [TxnImportController::class, 'showForm'])
+//     ->name('transactions.import.form');
 
-// // Process CSV and import
-Route::post('/transactions/import', [TxnImportController::class, 'import'])
-    ->name('transactions.import');
+// // // Process CSV and import
+// Route::post('/transactions/import', [TxnImportController::class, 'import'])
+//     ->name('transactions.import');
 
 
 // use App\Http\Controllers\ReportsShareController;
 
-Route::get('/members/update-totals', [\App\Http\Controllers\MemberTotalsController::class, 'recalculateAll'])->name('members.recalculate.totals');
+// Route::get('/members/update-totals', [\App\Http\Controllers\MemberTotalsController::class, 'recalculateAll'])->name('members.recalculate.totals');
 
 // // Route::get('/members/import-transactions', [MemberImportController::class, 'showImportTransactionsForm'])->name('members.import.transactions.form');
 // // Route::post('/members/import-transactions', [MemberImportController::class, 'importSavingsAndShares'])->name('members.import.transactions');
