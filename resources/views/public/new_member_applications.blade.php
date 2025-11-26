a9582e58ef8242fe93e0fda7a8320ce37840d3d0
Matthew Shahi <matthewshahi@gmail.com>
2025-11-26 12:53:22 +0300

    5678

--- resources/views/public/new_member_applications.blade.php
+++ resources/views/public/new_member_applications.blade.php
@@ -65,9 +65,12 @@
             </table>
         </div>
 
-        <div class="d-flex justify-content-center mt-4">
-            {{ $members->links() }}
-        </div>
+        <!-- Pagination -->
+<div class="d-flex justify-content-center mt-4">
+    {!! $members->onEachSide(1)->links('pagination::bootstrap-5') !!}
+</div>
+
+ 
     </div>
 </div>
 
