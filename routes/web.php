498eabb1efab157b5230e6a18e262697edd9638a
Matthew Shahi <matthewshahi@gmail.com>
2025-12-08 19:22:37 +0300

    kass

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,9 @@ Route::middleware(['auth'])->prefix('kass-migration')->group(function () {
 
         Route::post('/kass/process-all', [KassMigrationController::class, 'processAll'])
     ->name('kass.process.all');
+Route::get('/kass-migration/kass/process-all', 
+    [KassMigrationController::class, 'processAll']
+)->name('kass.process.all');
 
      
 Route::get('/kass/preview', [KassMigrationController::class, 'preview'])
