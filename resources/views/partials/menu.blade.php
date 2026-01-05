1166745df4a451bc2a92d609ae8c63be63ea85f2
Matthew Shahi <matthewshahi@gmail.com>
2026-01-05 09:36:28 +0300

    God, Please bless The Work of My Hands

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -364,6 +364,26 @@ $defaultLogoPath = '/image/logo.jpg'; // Default logo
                             <li class="Ul_li--hover">
                                 <a class="has-arrow" href="#">
                                     <i class="i-Bar-Chart text-20 me-2" style="color: #663399;"></i>
+                                    <span class="item-name text-15 text-muted">Other Savings /FOSA</span>
+                                </a>
+                                <ul class="mm-collapse">
+                                    <li class="item-name">
+                                        <a href="{{ url('reports/sasra/member_contributions/data') }}">
+                                            <span class="text-muted">Other Savings / Deposits</span>
+                                        </a>
+                                    </li>
+                                    
+
+
+
+                                </ul>
+                            </li>
+
+
+
+                            <li class="Ul_li--hover">
+                                <a class="has-arrow" href="#">
+                                    <i class="i-Bar-Chart text-20 me-2" style="color: #663399;"></i>
                                     <span class="item-name text-15 text-muted">Savings</span>
                                 </a>
                                 <ul class="mm-collapse">
