d5882bde5f969ccd50a67ebb0ec7b161f27787a5
matthewshahi <matthewshahi@gmail.com>
2026-01-15 09:48:40 +0300

    ThankYouLord

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -6,13 +6,13 @@
                 $currentDomain = parse_url(url('/'), PHP_URL_HOST);
 
                 // Check if it's localhost or 127.0.0.1
-if ($currentDomain === '127.0.0.1' || $currentDomain === 'localhost') {
-    $currentDomain = 'default'; // Use 'default' as a placeholder
-}
+                if ($currentDomain === '127.0.0.1' || $currentDomain === 'localhost') {
+                    $currentDomain = 'default'; // Use 'default' as a placeholder
+                }
 
-// Construct logo paths
-$domainLogoPath = '/image/' . $currentDomain . '.jpg'; // Domain-specific logo
-$defaultLogoPath = '/image/logo.jpg'; // Default logo
+                // Construct logo paths
+                $domainLogoPath = '/image/' . $currentDomain . '.jpg'; // Domain-specific logo
+                $defaultLogoPath = '/image/logo.jpg'; // Default logo
             @endphp
 
             @if (file_exists(public_path($domainLogoPath)))
@@ -173,6 +173,18 @@ $defaultLogoPath = '/image/logo.jpg'; // Default logo
                                     <span class="text-muted">Add/reduce</span>
                                 </a>
                             </li>
+                            <li class="item-name">
+                                <a href="{{ route('fosa.transactions.index') }}">
+                                    <span class="text-muted">Add/reduce</span>
+                                </a>
+                            </li>
+                            <li class="item-name">
+                                <a href="{{ route('fosa.transfers.index') }}">
+                                    <span class="text-muted">FOSA Out Transfers</span>
+                                </a>
+                            </li>
+
+
                             <li class="item-name">
                                 <a href="{{ route('fosa.transactions.import') }}">
                                     <span class="text-muted">Import</span>
@@ -353,7 +365,7 @@ $defaultLogoPath = '/image/logo.jpg'; // Default logo
                                         </a>
                                     </li>
 
-                                
+
 
                                     <li class="item-name">
                                         <a href="{{ url('/registration-fees') }}">
@@ -381,7 +393,7 @@ $defaultLogoPath = '/image/logo.jpg'; // Default logo
                                             <span class="text-muted">Other Savings </span>
                                         </a>
                                     </li>
-                                    
+
 
 
 
@@ -485,25 +497,25 @@ $defaultLogoPath = '/image/logo.jpg'; // Default logo
                                 </ul>
                             </li>
 
-                           <li class="Ul_li--hover">
-    <a class="has-arrow" href="#">
-        <i class="i-Bar-Chart text-20 me-2" style="color: #663399;"></i>
-        <span class="item-name text-15 text-muted">Transport</span>
-    </a>
+                            <li class="Ul_li--hover">
+                                <a class="has-arrow" href="#">
+                                    <i class="i-Bar-Chart text-20 me-2" style="color: #663399;"></i>
+                                    <span class="item-name text-15 text-muted">Transport</span>
+                                </a>
 
-    <ul class="mm-collapse">
+                                <ul class="mm-collapse">
 
-        <!-- MPESA Collections -->
-        <li class="item-name">
-            <a href="{{ url('/reports/transport/mpesa') }}">
-                <span class="text-muted">mPesa Collections</span>
-            </a>
-        </li>
+                                    <!-- MPESA Collections -->
+                                    <li class="item-name">
+                                        <a href="{{ url('/reports/transport/mpesa') }}">
+                                            <span class="text-muted">mPesa Collections</span>
+                                        </a>
+                                    </li>
 
-         
 
-    </ul>
-</li>
+
+                                </ul>
+                            </li>
 
 
                             <li class="Ul_li--hover">
@@ -656,13 +668,11 @@ $defaultLogoPath = '/image/logo.jpg'; // Default logo
             </div>
         </div>
     </div>
-    <div class="support-contact p-2 d-flex justify-content-between align-items-center"
-        style="position: absolute; bottom: 0; left: 0; width: 100%; background: linear-gradient(to right, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)); 
+    <div class="support-contact p-2 d-flex justify-content-between align-items-center" style="position: absolute; bottom: 0; left: 0; width: 100%; background: linear-gradient(to right, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)); 
             font-size: 14px; border-top: 1px solid rgba(102, 51, 153, 0.2);">
         <div style="font-size: 13px; color: rgba(102, 51, 153, 1); font-weight: 600;">
             <strong>ERP provided by:</strong> <br> Shahi Services,
-            <a href="tel:+254722400737"
-                style="text-decoration: none; color: rgba(102, 51, 153, 1); font-weight: bold;">
+            <a href="tel:+254722400737" style="text-decoration: none; color: rgba(102, 51, 153, 1); font-weight: bold;">
                 +254722400737
             </a>
         </div>
