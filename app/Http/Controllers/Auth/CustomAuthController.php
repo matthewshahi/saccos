dd924f4ab84736e33c2840546b3d249aebabf975
matthewshahi <matthewshahi@gmail.com>
2026-02-01 13:03:35 +0300

    Oh Jesus

--- app/Http/Controllers/Auth/CustomAuthController.php
+++ app/Http/Controllers/Auth/CustomAuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Member;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class CustomAuthController extends Controller
 {
