6daa20fdf7ba2b7b649df00189c49cff91563bf1
matthewshahi <matthewshahi@gmail.com>
2026-02-26 21:05:31 +0300

    Jesus

--- resources/views/dashboard/member_dashboard.blade.php
+++ resources/views/dashboard/member_dashboard.blade.php
@@ -4,7 +4,7 @@
 
 @include("member_name")
 
-@include('partials.junior-context-banner', ['data' => $data ?? null])
+@include('dashboard.junior-context-banner', ['data' => $data ?? null])
 @include('dashboard.payment_options')
  
 
