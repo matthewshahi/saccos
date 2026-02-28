8cd1a3ad16641f935285ae17ddbb56062da0ca61
matthewshahi <matthewshahi@gmail.com>
2026-02-28 15:17:36 +0300

    Loans

--- app/Http/Controllers/Api/LoanApplicationController.php
+++ app/Http/Controllers/Api/LoanApplicationController.php
@@ -44,7 +44,7 @@ class LoanApplicationController extends Controller
         */
         $loanTypes = DB::table('sacco_loan_types')
             ->where('loan_type_deleted', 'N')
-            ->where('loan_type_guaranteable_percent', 0)
+            // ->where('loan_type_guaranteable_percent', 0)
             ->orderBy('loan_type_name')
             ->get()
             ->map(function ($t) {
