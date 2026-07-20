025667b41545ac1c7967c224c80a947cac9ee893
matthewshahi <matthewshahi@gmail.com>
2026-07-20 16:54:46 +0300

    yeslord

--- config/app.php
+++ config/app.php
@@ -210,7 +210,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\SaccoMenuServiceProvider::class,
     ])->toArray(),
 
     /*
